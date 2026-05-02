47dabdc175be51de2058fc3e068f6fb0eee405e0
Kajal sharma <shrmakajal963@gmail.com>
2026-05-02 12:05:04 +0545

    fix CI indentation issue

--- conftest.py
+++ conftest.py
@@ -1,36 +1,22 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+import os
 
-# -------------------------
-# BASIC DRIVER FIXTURE
-# -------------------------
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(
         service=Service(ChromeDriverManager().install()),
         options=options
     )
 
-    driver.get("https://www.saucedemo.com/")
-
     yield driver
     driver.quit()
-
-
-# -------------------------
-# LOGIN FIXTURE
-# -------------------------
-@pytest.fixture
-def logged_in_driver(driver):
-    driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
-
-    return driver
