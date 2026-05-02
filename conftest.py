685d2ceff1cae95f11c27f367e756f1bcbfe0d95
Kajal sharma <shrmakajal963@gmail.com>
2026-05-02 12:08:40 +0545

    updated conftest

--- conftest.py
+++ conftest.py
@@ -1,22 +1,18 @@
+import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-
-import os
 
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
 
+    # CI safe settings
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
+    # IMPORTANT: no ChromeDriverManager here
+    driver = webdriver.Chrome(options=options)
 
     yield driver
     driver.quit()
